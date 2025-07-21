21030198af432595b087d176f84cbb0166313cc0
NicholasPierce99 <piercnic@gmail.com>
2025-07-21 18:42:51 -0400

    update weather routes

--- api/routes/weather-routes.go
+++ api/routes/weather-routes.go
@@ -1,13 +1,30 @@
 package routes
 
 import (
+	"encoding/json"
 	"naptalie-api/api/helpers"
+	"naptalie-api/api/types"
 	"net/http"
 )
 
-func GetWeatherRoute(w http.ResponseWriter, req *http.Request) {
+func HandleDiscordWebhookWeather(w http.ResponseWriter, r *http.Request) {
+	// Set headers for JSON response
+	w.Header().Set("Content-Type", "application/json")
+
 	baseUrl := "https://api.open-meteo.com/v1/forecast?"
 
-	helpers.GetWeather(baseUrl)
+	getWeatherData := helpers.GetWeather(baseUrl)
+	// Your logic here
+	response := types.Response{
+		Message: "Command executed successfully",
+		Data:    getWeatherData.Data,
+		Success: true,
+	}
 
+	// Encode and send JSON response
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
