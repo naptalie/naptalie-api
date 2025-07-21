b96d3a57ff20a320923c93cffdc437bbadf1d9fe
NicholasPierce99 <piercnic@gmail.com>
2025-07-21 18:43:50 -0400

    introduce more types

--- api/types/types.go
+++ api/types/types.go
@@ -35,6 +35,41 @@ type ForecastRequest struct {
 	Time     TimeParameters `json:"time"`
 }
 
+type Response struct {
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+	Success bool   `json:"success"`
+}
+
+// WeatherData represents the inner weather API response structure
+type WeatherData struct {
+	Latitude         float64       `json:"latitude"`
+	Longitude        float64       `json:"longitude"`
+	GenerationTimeMs float64       `json:"generationtime_ms"`
+	UTCOffsetSeconds int           `json:"utc_offset_seconds"`
+	Timezone         string        `json:"timezone"`
+	TimezoneAbbr     string        `json:"timezone_abbreviation"`
+	Elevation        float64       `json:"elevation"`
+	DailyUnits       DailyUnits    `json:"daily_units"`
+	Daily            DailyForecast `json:"daily"`
+}
+
+type DailyUnits struct {
+	Time          string `json:"time"`
+	TempMax       string `json:"temperature_2m_max"`
+	TempMin       string `json:"temperature_2m_min"`
+	Precipitation string `json:"precipitation_sum"`
+	WeatherCode   string `json:"weathercode"`
+}
+
+type DailyForecast struct {
+	Time          []string  `json:"time"`
+	TempMax       []float64 `json:"temperature_2m_max"`
+	TempMin       []float64 `json:"temperature_2m_min"`
+	Precipitation []float64 `json:"precipitation_sum"`
+	WeatherCode   []int     `json:"weathercode"`
+}
+
 // Unit constants
 const (
 	TempCelsius    = "celsius"
